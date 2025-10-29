98953fc11ff01b3906ef833cad5a206587d8f005
Mikhail Goncharov <chack4993@gmail.com>
2025-10-29 22:38:19 +0300

    Добавлен интерфейс и класс контекст. Добавлена строка подключения

---
+++ Infrastructure/Data/DataBaseContext/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Application.Data.DataBaseContext;
+using Domain.Models;
+using Domain.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data.DataBaseContext
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public DbSet<Topic> Topics => Set<Topic>();
+    }
+}
