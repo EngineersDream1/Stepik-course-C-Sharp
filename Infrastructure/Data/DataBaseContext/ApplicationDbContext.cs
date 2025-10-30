b3c5acf7865d2cc21c32b0f347bc416dff08d51c
Mikhail Goncharov <chack4993@gmail.com>
2025-10-30 22:15:25 +0300

    Настройка хранения сущностей. Миграция

--- Infrastructure/Data/DataBaseContext/ApplicationDbContext.cs
+++ Infrastructure/Data/DataBaseContext/ApplicationDbContext.cs
@@ -8,5 +8,29 @@ namespace Infrastructure.Data.DataBaseContext
     public class ApplicationDbContext : DbContext, IApplicationDbContext
     {
         public DbSet<Topic> Topics => Set<Topic>();
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Topic>()
+                .Property(topic => topic.Id)
+                .HasConversion(
+                    id => id.Value,
+                    value => TopicId.Of(value)
+                );
+
+            modelBuilder.Entity<Topic>()
+                .OwnsOne(Topic => Topic.Location, location =>
+                {
+                    location.Property(l => l.City).HasColumnName("City");
+                    location.Property(l => l.Street).HasColumnName("Street");
+                });
+        }
     }
 }
