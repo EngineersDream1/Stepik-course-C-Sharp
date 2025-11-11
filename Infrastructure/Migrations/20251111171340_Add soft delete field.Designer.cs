8bb98977543909714d589649e1ef5daafc2695f4
Mikhail Goncharov <chack4993@gmail.com>
2025-11-11 20:26:36 +0300

    Добавлено удаление (soft delete)

--- Infrastructure/Migrations/20251111171340_Add soft delete field.Designer.cs
+++ Infrastructure/Migrations/20251111171340_Add soft delete field.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Infrastructure.Data.DataBaseContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251111171340_Add soft delete field")]
+    partial class Addsoftdeletefield
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.0");
@@ -28,7 +31,7 @@ namespace Infrastructure.Migrations
                     b.Property<DateTime?>("EventStart")
                         .HasColumnType("TEXT");
 
-                    b.Property<bool>("IsDeleted")
+                    b.Property<bool>("IsDelted")
                         .HasColumnType("INTEGER");
 
                     b.Property<string>("Summary")
---
+++ Infrastructure/Migrations/20251111172028_Correct soft delete field.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Correctsoftdeletefield : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsDelted",
+                table: "Topics",
+                newName: "IsDeleted");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsDeleted",
+                table: "Topics",
+                newName: "IsDelted");
+        }
+    }
+}
