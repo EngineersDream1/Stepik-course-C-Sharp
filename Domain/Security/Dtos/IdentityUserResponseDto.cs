86bfa9b74d277d4486b1b47db9cda5671fd214de
Mikhail Goncharov <chack4993@gmail.com>
2026-01-15 21:41:34 +0300

    Добавление необходимых Security DTO

---
+++ Domain/Security/Dtos/IdentityUserResponseDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Security.Dtos
+{
+    public record IdentityUserResponseDto(
+        string Username,
+        string Email,
+        string JwtToken        
+    );
+}
---
+++ Domain/Security/Dtos/RegisterUserRequestDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Security.Dtos
+{
+    public record RegisterUserRequestDto(
+        string FullName,
+        string Username,
+        string Email,
+        string Password
+    );
+}
