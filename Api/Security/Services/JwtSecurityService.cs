c7c126cf8e73690a8713f02f5d1cf812ae9c2ba2
Mikhail Goncharov <chack4993@gmail.com>
2026-01-17 18:23:38 +0300

    Добавлены Claims и реализован метод создания JWT токена

--- Api/Security/Services/JwtSecurityService.cs
+++ Api/Security/Services/JwtSecurityService.cs
@@ -1,4 +1,8 @@
 ﻿using Domain.Security;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
 
 namespace Api.Security.Services
 {
@@ -7,7 +11,33 @@ namespace Api.Security.Services
         public string CreateToken(CustomIdentityUser user)
         {
             string secretKey = configuration["AuthSettings:SecretKey"]!;
-            throw new NotImplementedException();
+
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+                new Claim(JwtRegisteredClaimNames.Name, user.UserName!),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email!),
+                new Claim("is_premium", "true")
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenHandler = new JsonWebTokenHandler();
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                SigningCredentials = creds,
+                Subject = new ClaimsIdentity(claims),
+                IssuedAt = DateTime.UtcNow,
+                NotBefore = DateTime.UtcNow.AddMinutes(0),
+                Expires = DateTime.UtcNow.AddMinutes(1)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return token;
         }
     }
 }
