e0b458cc9f82fa24795cba5bddcedb86a01c041a
Mikhail Goncharov <chack4993@gmail.com>
2026-01-17 17:14:23 +0300

    Добавлен контроллер авторизации пользователя (login)

---
+++ Api/Controllers/AuthController.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Security;
+using Domain.Security.Dtos;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController(UserManager<CustomIdentityUser> manager) : ControllerBase
+    {
+        [HttpPost("login")]
+        public async Task<IResult> login(LoginRequest dto)
+        {
+            var user = await manager.FindByEmailAsync(dto.Email);
+
+            if (user == null)
+            {
+                return Results.Unauthorized();
+            }
+
+            var result = await manager.CheckPasswordAsync(user, dto.Password);
+
+            if(result)
+            {
+                var response = new IdentityUserResponseDto(
+                    user.UserName!,
+                    user.Email!,
+                    "jwt"
+                );
+
+                return Results.Ok(new {result = response});
+            }
+
+            return Results.Unauthorized();
+        }
+    }
+}
